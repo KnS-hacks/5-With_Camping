4c7900e6f3ce638cbb11d897169028d72e2e8ea6
윤병수 <qudtn990@likelion.org>
2021-11-19 01:21:06 +0900

    [BE][수정] :: 회원가입, 로그인 수정 (병수)

--- with_camping/member/urls.py
+++ with_camping/member/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from . import views
+from .import views
 
 urlpatterns = [
-    path('', views.login, name='member_login'),
+    path('', views.member_login, name='member_login'),
     path('signup/', views.sign_up, name='sign_up'),
-    path('logout/', views.logout, name='member_logout'),
+    path('logout/', views.member_logout, name='member_logout'),
 ]
