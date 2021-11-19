f28bffb30db8ca0106e07df9c4cd7ffb7d0b1289
desfox <desfox99@naver.com>
2021-11-20 04:58:07 +0900

    [BE]::커뮤니티 이미지 static 구현 완료

--- with_camping/community/urls.py
+++ with_camping/community/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,4 +8,6 @@ urlpatterns = [
     path('', views.camping_community, name="camping_community"),
     path('free/', views.free, name="free_bulletin"),
     path('write/', views.write, name="write"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
